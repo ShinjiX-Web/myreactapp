1953a275b97f6e6330a25df78a0ba7ccad54dc68
Nate <npanares4@egmail.com>
2023-02-25 17:40:41 +0800

    V2.0

--- src/Content.js
+++ src/Content.js
@@ -2,17 +2,27 @@ import './Content.css';
 
 const Content = () => {
 
-  const handleNameChanges = 'Peter';
-
   return (
     <main>
-      <p className='mysample'>
-        Hello {handleNameChanges}!
-      </p>
-    </main>
+      <div className='container my-5'>
+        <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+          <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h1 className="display-4 fw-bold lh-1 header-one">Border hero with cropped image and shadows</h1>
+            <p className="lead my-3"> Quickly design and customize responsive mobile-first sites with Bootstrap,
+              the world’s most popular front-end open source toolkit, featuring Sass variables and mixins,
+              responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+              <button type="button" className="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Primary</button>
+              <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+            </div>
+          </div>
+          <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+            <img className="rounded-lg-3" src="https://getbootstrap.com/docs/5.3/examples/heroes/bootstrap-themes.png" alt="bootstrap" width="720"></img>
+          </div>
+        </div >
+      </div >
+    </main >
   )
-
-
 }
 
 export default Content;

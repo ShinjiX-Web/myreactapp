b907af8bee9b3167f31e07894b7d5b256713a7f9
ShinjiX-Web <npanares4@gmail.com>
2023-02-26 13:56:52 +0800

    VersionFinal

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,4 +1,8 @@
 import './Content.css';
+import { ReactComponent as Collection } from "./collection.svg";
+import { ReactComponent as GearFill } from "./gearfill.svg";
+import { ReactComponent as Speedometer } from "./speedometer.svg";
+import { ReactComponent as Table } from "./table.svg";
 
 const Content = () => {
 
@@ -20,6 +24,139 @@ const Content = () => {
           </div>
         </div >
       </div >
+
+      <div class="b-example-divider"></div>
+
+        <div class="container px-4 py-5" id="custom-cards">
+
+          <h2 class="pb-2 border-bottom">Recent Blogs</h2>
+
+          <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+          
+            <div class="col">
+            
+              <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg card-one">
+                <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                  <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Journey to the Moon</h3>
+                  <ul class="d-flex list-unstyled mt-auto">
+                    <li class="me-auto">
+                      <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white"></img>
+                    </li>
+                    <li class="d-flex align-items-center me-3">
+                      <svg class="bi me-2" width="1em" height="1em"></svg>
+                      <small>Earth</small>
+                    </li>
+                    <li class="d-flex align-items-center">
+                      <svg class="bi me-2" width="1em" height="1em"></svg>
+                      <small>3d</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg card-two">
+                <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                  <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">A flower to the garden in the sky that melt our hearts</h3>
+                  <ul class="d-flex list-unstyled mt-auto">
+                    <li class="me-auto">
+                      <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white"></img>
+                    </li>
+                    <li class="d-flex align-items-center me-3">
+                      <svg class="bi me-2" width="1em" height="1em"></svg>
+                      <small>Pakistan</small>
+                    </li>
+                    <li class="d-flex align-items-center">
+                      <svg class="bi me-2" width="1em" height="1em"></svg>
+                      <small>4d</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg card-three">
+                <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                  <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h3>
+                  <ul class="d-flex list-unstyled mt-auto">
+                    <li class="me-auto">
+                      <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white"></img>
+                    </li>
+                    <li class="d-flex align-items-center me-3">
+                      <svg class="bi me-2" width="1em" height="1em"></svg>
+                      <small>California</small>
+                    </li>
+                    <li class="d-flex align-items-center">
+                      <svg class="bi me-2" width="1em" height="1em"></svg>
+                      <small>5d</small>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="b-example-divider"></div>
+
+        <div class="container px-4 py-5">
+          <h2 class="pb-2 border-bottom">Another exciting Moment</h2>
+
+          <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5 left-aligned">
+            <div class="col d-flex flex-column align-items-start gap-2">
+              <h3 class="fw-bold">Left-aligned title explaining these awesome features</h3>
+              <p class="text-muted">Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+              <a href="https://go.dev/" class="btn btn-primary btn-lg">Primary button</a>
+            </div>
+
+            <div class="col">
+              <div class="row row-cols-1 row-cols-sm-2 g-4">
+                <div class="col d-flex flex-column gap-2">
+                  <div class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                    <svg class="small" width="1em" height="1em">
+                    <GearFill></GearFill>
+                    </svg>
+                  </div>
+                  <h4 class="fw-semibold mb-0">Featured title</h4>
+                  <p class="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+                </div>
+
+                <div class="col d-flex flex-column gap-2">
+                  <div class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                    <svg class="small" width="1em" height="1em">
+                      <Collection></Collection>
+                      </svg>                               
+                  </div>
+                  <h4 class="fw-semibold mb-0">Featured title</h4>
+                  <p class="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+                </div>
+
+                <div class="col d-flex flex-column gap-2">
+                  <div class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                    <svg class="bi" width="1em" height="1em">
+                     <Speedometer></Speedometer>
+                    </svg>
+                  </div>
+                  <h4 class="fw-semibold mb-0">Featured title</h4>
+                  <p class="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+                </div>
+
+                <div class="col d-flex flex-column gap-2">
+                  <div class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                    <svg class="bi" width="1em" height="1em">
+                      <Table></Table>
+                    </svg>
+                  </div>
+                  <h4 class="fw-semibold mb-0">Featured title</h4>
+                  <p class="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </main >
   )
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
 
             <div className="container">
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top extra-one">
+            <footer className="d-flex flex-wrap justify-content-center justify-content-sm-between align-items-center py-3 my-4 border-top extra-one">
                 <div className="col-md-4 d-flex align-items-center">
                 <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"><Logo></Logo></a>
                 <span className="mb-3 mb-md-0">&copy; 2023 X-Rated Films, Inc</span>
